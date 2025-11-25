993d539fb3f525a6ffae0e6ae67bfdf31a3251cb
Jasubrek Yuldashov <jasurbekyuldashov123@gmail.com>
2025-11-25 22:30:11 +0500

    push

---
+++ dargohbook2/ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [
+    {
+      name: 'dargohbook',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 8072',
+      cwd: '/Users/jasurbekyuldashov/Desktop/dargoh/dargohbook2',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 8072,
+        DB_HOST: 'localhost',
+        DB_PORT: 5437,
+        DB_USERNAME: 'postgres',
+        DB_PASSWORD: 'password',
+        DB_NAME: 'dargohbook_secret_2024',
+        BILLZ_API_URL: 'https://dargox.billz.io/api/v2',
+        BILLZ_PLATFORM_ID: '7d4a4c38-dd84-4902-b744-0488b80a4c01',
+        BILLZ_PHONE: '+998951590102',
+        BILLZ_PASSWORD: 'sardor0102',
+        SYNC_INTERVAL: 30,
+      },
+    },
+  ],
+};
